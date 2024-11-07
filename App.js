aa9c0ec560c6d02d6ee56e11e17f170fead7a178
Johannes <johannes.widkopp@gmail.com>
2024-11-07 13:54:45 +0200

    korjattu merge konfliktit

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-
-import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import Header from './components/Header'
+import Footer from './components/Footer';
+import useTheme from './hooks/Theme'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ListScreen from './screens/ListScreen';
 import ItemScreen from './screens/ChosenListScreen';
 
+
+//--------------Joonan dark ja light mode---------------//
 /* export default function App() {
 
   const [isDarkTheme, setIsDarkTheme] = useState(false);
@@ -19,13 +25,54 @@ import ItemScreen from './screens/ChosenListScreen';
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-} */
+    <SafeAreaProvider>
+      <PaperProvider theme={theme}>
+        <SafeAreaView style={styles.safeArea}>
+          <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
 
+            <Header style={styles.header} />
+
+            <View style={[styles.content, { backgroundColor: theme.colors.primary }]}>
+              <Text>Open up App.js to start working on your app!</Text>
+              <TouchableOpacity onPress={toggleTheme}>
+                <Icon name="dark-mode" size={30} color={theme.colors.toggleButtonColor} />
+              </TouchableOpacity>
+            </View>
+
+            <Footer style={styles.footer} />
+
+          </View>
+        </SafeAreaView>
+      </PaperProvider>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    height: 50,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '90%',
+    marginTop: 10,
+    marginBottom: 40,
+  },
+  footer: {
+    height: 50,
+  },
+}); */
+//--------------Joonan dark ja light mode---------------//
 
 const Stack = createStackNavigator();
 

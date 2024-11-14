1e4df3b0b74e0cf97a2bfa00c35f46d096e3320e
Jarno Tauriainen <t2taja00@students.oamk.fi>
2024-11-14 13:14:43 +0200

    package lock

--- App.js
+++ App.js
@@ -1,40 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { Provider as PaperProvider } from 'react-native-paper'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import React, { useState } from 'react'
 import useTheme from './hooks/Theme'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Footer from './components/Footer'
+import Homescreen from './screens/HomeScreen'
+import WeatherScreen from './screens/WeatherScreen'
+import ListScreen from './screens/ListScreen'
+import ItemScreen from './screens/ChosenListScreen'
+import CurrencyCalculator from './screens/CalculatorScreen'
 
-import Footer from './components/Footer';
-import Homescreen from './screens/HomeScreen';
-import WeatherScreen from './screens/WeatherScreen';
-import ListScreen from './screens/ListScreen';
-import ItemScreen from './screens/ChosenListScreen';
-import CurrencyCalculator from './screens/CalculatorScreen';
 
-
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default function App() {
 
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false)
 
   const theme = useTheme(isDarkTheme)
 
   const toggleTheme = () => {
-    setIsDarkTheme(prevTheme => !prevTheme);
-  };
+    setIsDarkTheme(prevTheme => !prevTheme)
+  }
 
   return (
     <NavigationContainer>
       <PaperProvider theme={theme}>
         <SafeAreaView style={styles.safeArea}>
-
-
-
-
           <Stack.Navigator initialRouteName='Home' screenOptions={{
             headerStyle: { height: 70, backgroundColor: theme.colors.primary },
             headerTitleAlign: 'center',
@@ -49,14 +44,11 @@ export default function App() {
             <Stack.Screen style={styles.screen} name="Lists" component={ListScreen} />
             <Stack.Screen style={styles.screen} name="Items" component={ItemScreen} />
           </Stack.Navigator>
-
-
           <Footer style={styles.footer} />
-
         </SafeAreaView>
       </PaperProvider>
     </NavigationContainer>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -76,6 +68,6 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 40,
   },
-  
-});
+
+})
 /*-----------Testausta varten-------------- */
